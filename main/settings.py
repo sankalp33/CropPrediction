dcf6b561adeaa58d3e35171a0029bac75d581ad9
Parth Mistry <parthmi98@gmail.com>
2021-05-26 23:55:47 +0530

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -120,9 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
-STATIC_URL = '/static/'
+STATIC_URL = 'crops/static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'crops/static')
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 
